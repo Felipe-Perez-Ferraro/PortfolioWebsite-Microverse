fb509ee33952aeb90e8c2dc926901f9c49875746
Feliperto <fperezferraro@gmail.com>
2023-06-06 19:01:46 -0300

    update js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const workButton = document.getElementsByClassName('work__button');
 const aboutButton = document.querySelector('.about__button');
 
-for (let i = 0; i < workButton.length; i+=1) {
+for (let i = 0; i < workButton.length; i += 1) {
   workButton[i].addEventListener('click', () => {
     if (workButton[i].id === 'work1') {
       workButton[i].classList.toggle('press');
@@ -23,8 +23,6 @@ aboutButton.addEventListener('click', () => {
   aboutButton.classList.toggle('press');
 });
 
-window.onscroll = () => scrollFun();
-
 const scrollFun = () => {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.body.classList.add('scroll');
@@ -32,3 +30,5 @@ const scrollFun = () => {
     document.body.classList.remove('scroll');
   }
 };
+
+window.onscroll = () => scrollFun();
