01c9eda09057f558d445ee6afeaa917ce87d8b44
Felipe <fperezferraro@gmail.com>
2023-06-08 17:36:43 -0300

    adding .eslintrc.json file

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'extends': 'gsap',
+    'parserOptions': {
+        'ecmaVersion': 12
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
